0a647f3bfb4dab097c55e3a016924b92b6545442
Filip Stachura <filip@appsilon.pl>
2016-11-29 20:25:06 +0100

    Dropdown fixes - state of generated components is persistent.

--- R/shiny.R
+++ R/shiny.R
@@ -4,10 +4,13 @@
   shiny::addResourcePath('semanticui', system.file('www', package = 'semanticui', mustWork = TRUE))
 }
 
-shiny_input <- function(input_id, shiny_ui) {
+shiny_input <- function(input_id, shiny_ui, value = NULL) {
+  selected <- shiny::restoreInput(id = input_id, default = value)
+
   custom_input_class <- 'shiny-custom-input'
   shiny_ui$attribs$class <- paste(custom_input_class, ifelse(is.null(shiny_ui$attribs$class), "", shiny_ui$attribs$class))
   shiny_ui$attribs$id <- input_id
+  shiny_ui$attribs[['data-value']] <- selected
 
   shiny::tagList(
     shiny::singleton(
