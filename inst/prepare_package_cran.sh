7e10d76442355ef3a566d00caed99651c7b152da
krystian <krystian8207@gmail.com>
2017-05-15 18:11:48 +0200

    Fixing release script

--- inst/prepare_package_cran.sh
+++ inst/prepare_package_cran.sh
@@ -1,22 +1,23 @@
 echo "==> Switching to cran-release branch."
 cd ..
-git checkout -B cran-release
+git checkout -B temp_cran_release
 
 echo "==> Preparing package for CRAN release."
 rm -r docs/ examples/ CHANGELOG.md README.Rmd README_files build_readme.R examples/ readme_assets/ readme_rmd_template/ inst/semantic/.versions
 
 # Debug comment to identify which files removed.
 git status
-
+mkdir build
 
 echo "==> Building package tar.gz archive."
 R CMD build ../shiny.semantic
 zip_file=$(find *tar.gz)
 
 echo "==> Checking package."
-R CMD Rd2pdf $zip_file
 R CMD check --as-cran $zip_file
+mv $zip_file build/
 
 echo "==> Reset."
-git reset --hard
-# git checkout master
+#git reset --hard
+#git checkout master
+#git branch -d temp_cran_release
