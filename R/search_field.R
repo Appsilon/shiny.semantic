fd9d3cd30187fa796085b7a242c125127775cdd8
Krystian Igras <krystian8207@gmail.com>
2017-05-29 16:25:26 +0200

    Added example for search_field function.

--- R/search_field.R
+++ R/search_field.R
@@ -1,4 +1,3 @@
-
 #' Create search field Semantic UI component
 #'
 #' This creates a default search field using Semantic UI styles with Shiny input. Search field is already initialized
@@ -10,6 +9,49 @@
 #' @param search_api_url Register custom API url with server JSON Response containing fields `title` and `description`.
 #' @param default_text Text to be visible on serach field when nothing is selected.
 #' @param value Pass value if you want to initialize selection for search field.
+#'
+#' @examples 
+#' ## Only run examples in interactive R sessions
+#' if (interactive()) {
+#' library(shiny)
+#' library(shiny.semantic)
+#' library(gapminder)
+#' library(dplyr)
+#'
+#' ui <- function() {
+#'   shinyUI(
+#'     semanticPage(
+#'       title = "Dropdown example",
+#'       uiOutput("search_letters"),
+#'       p("Selected letter:"),
+#'       textOutput("selected_letters")
+#'    )
+#'   )
+#' }
+#'
+#' server <- shinyServer(function(input, output, session) {
+#'  
+#'  search_api <- function(gapminder, q){
+#'    has_matching <- function(field) {
+#'      startsWith(field, q)
+#'    }
+#'    gapminder %>%
+#'      mutate(country = as.character(country)) %>%
+#'      select(country) %>%
+#'      unique %>%
+#'      filter(has_matching(country)) %>%
+#'      head(5) %>%
+#'      transmute(title = country,
+#'                description = country)
+#'  }
+#'  
+#'  search_api_url <- register_search(session, gapminder, search_api)
+#'  output$search_letters <- shiny::renderUI(search_field("search_result", search_api_url))
+#'  output$selected_letters <- renderText(input[["search_result"]])
+#' })
+#'}
+#'
+#'shinyApp(ui = ui(), server = server)
 #'
 #' @export
 #' @importFrom magrittr "%>%"
