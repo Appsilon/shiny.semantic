00c504528a1b143d7c75d3375a82cb7a5241c808
Pedro Silva <pedro@appsilon.com>
2019-05-13 13:43:23 +0200

    Added content vs elipsis example.

--- R/modal.R
+++ R/modal.R
@@ -84,6 +84,30 @@
 #'   })
 #' })
 #'
+#' ## Changing attributes of header and content.
+#' library(shiny)
+#' library(shiny.semantic)
+#'
+#' ui <- function() {
+#'   shinyUI(
+#'     semanticPage(
+#'       actionButton("show", "Show modal dialog")
+#'     )
+#'   )
+#' }
+#'
+#' server = function(input, output) {
+#'   observeEvent(input$show, {
+#'     create_modal(modal(
+#'       id = "simple-modal",
+#'       title = "Important message",
+#'       header = list(style = "background: lightcoral"),
+#'       content = list(style = "background: lightblue", `data-custom` = "value", "This is an important message!"),
+#'       p("This is also part of the content!")
+#'     ))
+#'   })
+#' }
+#'
 #' @import shiny
 #' @export
 modal <- function(...,
