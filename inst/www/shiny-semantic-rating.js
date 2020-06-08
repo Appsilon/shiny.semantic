852f2f9c6994ef1273da34fb946ea0dab0741e2b
Krystian Igras <krystian8207@gmail.com>
2020-06-08 14:00:43 +0200

    Remove debuggers.

--- inst/www/shiny-semantic-rating.js
+++ inst/www/shiny-semantic-rating.js
@@ -1,25 +1,19 @@
 var semanticRatingBinding = new Shiny.InputBinding();
 $.extend(semanticRatingBinding, {
   find: function(scope) {
-    console.log('find');
     return $(scope).find(".rating");
   },
   initialize: function(el){
-    console.log('initialize');
     $("#" + el.id).rating('setting', 'clearable', true);
   },
   getValue: function(el) {
-    console.log('get value');
     return $(el).data('rating') || 0;
   },
   setValue: function(el, value) {
-    console.log('set value');
     $(el).rating('set rating', value);
   },
   subscribe: function(el, callback) {
-    console.log('subscribe');
     $(el).rating('setting', 'onRate', function(value) {
-      console.log('click');
       var $el = $(this);
       var val = $el.rating('get rating') || 0;
       $el.data('rating', val);
