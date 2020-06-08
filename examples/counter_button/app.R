2126d53379c11907cd9327e869faf3a3eb1b64db
Dominik Krzeminski <dominik@appsilon.com>
2020-06-08 20:48:08 +0100

    modified counter button example

--- examples/counter_button/app.R
+++ examples/counter_button/app.R
@@ -1,45 +1,18 @@
 library(shiny)
 library(shiny.semantic)
-big_mark_reqex <- function(big_mark) {
-  if (big_mark == " ") {
-    return("\\s")
-  } else {
-    return(big_mark)
-  }
-}
 
-counter_button <- function(inputId, label, icon, value = 0, color = "", big.mark = " ") {
-  big_mark_regex <- big_mark_reqex(big.mark)
-  shiny::div(
-    class = "ui labeled button", tabindex = "0",
-    shiny::tagList(
-      uibutton(name = inputId, label, icon, class = color, `data-val` = value),
-      shiny::tags$span(class = glue::glue("ui basic {color} label"), format(value, big.mark = big.mark)),
-      shiny::tags$script(HTML(
-        glue::glue("$('#{inputId}').on('click', function() {{
-          let $label = $('#{inputId} + .label')
-          let value = parseInt($label.html().replace(/{big_mark_regex}/g, ''))
-          $label.html((value + 1).toString().replace(/\\B(?=(\\d{{3}})+(?!\\d))/g, '{big.mark}'))
-        }})")
-      ))
-    )
-  )
-}
+ui <-semanticPage(
+     counterbutton("counter", "How many dogs?",
+                   icon = uiicon("dog"),
+                   size = "big", color = "green"),
+     h1("Dog count:"),
+     h2(shiny::textOutput("dogs"))
+ )
 
-ui <- function() {
-  shinyUI(
-    semanticPage(
-      counter_button("meows", "Meow", uiicon("cat"), value = 998, color = "red"),
-      textOutput("meows")
-    )
-  )
-}
-
-server <- shinyServer(function(input, output, session) {
-  output$meows <- renderText({
-    glue::glue("Total Meows: ", input$meows)
+server <- function(input, output) {
+  output$dogs <- renderText({
+    input$counter
   })
-})
+}
 
-shinyApp(ui = ui(), server = server)
-
+shinyApp(ui, server)
