92755515196e81a8257753bdcb5d1d266961894f
Olga Mierzwa-Sulima <olga.mierzwa@gmail.com>
2020-07-02 09:56:12 +0200

    Make example shorter

--- R/button.R
+++ R/button.R
@@ -76,33 +76,20 @@ actionButton <- function(inputId, label, icon = NULL, width = NULL, ...) {
 #' library(shiny.semantic)
 #'
 #' ui <- semanticPage(
-#'   actionButton("update", "Update other buttons"),
+#'   actionButton("update", "Update button"),
 #'   br(),
-#'   actionButton("goButton", "Go"),
-#'   br(),
-#'   actionButton("goButton2", "Go 2", icon = icon("home")),
-#'   br(),
-#'   actionButton("goButton3", "Go 3")
+#'   actionButton("go_button", "Go")
 #' )
 #'
 #' server <- function(input, output, session) {
 #'   observe({
 #'     req(input$update)
 #'
-#'     # Updates goButton's label and icon
-#'     updateActionButton(session, "goButton",
+#'     # Updates go_button's label and icon
+#'     updateActionButton(session, "go_button",
 #'                        label = "New label",
 #'                        icon = icon("calendar"))
 #'
-#'     # Leaves goButton2's label unchaged and
-#'     # removes its icon
-#'     updateActionButton(session, "goButton2",
-#'                        icon = character(0))
-#'
-#'     # Leaves goButton3's icon, if it exists,
-#'     # unchaged and changes its label
-#'     updateActionButton(session, "goButton3",
-#'                        label = "New label 3")
 #'   })
 #' }
 #' shinyApp(ui, server)
