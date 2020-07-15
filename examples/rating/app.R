e804f46ddf516132ab94ddd829286cfc15f6ccb5
Dominik Krzeminski <dominik@appsilon.com>
2020-07-15 10:59:44 +0100

    updated rating example

--- examples/rating/app.R
+++ examples/rating/app.R
@@ -5,11 +5,11 @@ ui <- function() {
   shinyUI(
     semanticPage(
       title = "Rating example",
-      rating("rating", "How much do you like this example?",
+      rating_input("rating", "How much do you like this example?",
              icon = "heart", max = 8, color = "green", size = "huge"),
       p("Your rate:"),
       textOutput("your_rate"),
-      uibutton("update", "Update rate")
+      button("update", "Update rate")
     )
   )
 }
@@ -18,7 +18,7 @@ server <- shinyServer(function(input, output, session) {
   output$your_rate <- renderText(input[["rating"]])
 
   observeEvent(input$update, {
-    updateRating(session, "rating", "New label", 2)
+    update_rating_input(session, "rating", "New label", 2)
   }, ignoreInit = TRUE)
 })
 
