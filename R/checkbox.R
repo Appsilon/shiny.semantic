c425d4e0f1d6df5edb0c3685f300ce12176e9f63
Olga Mierzwa-Sulima <olga.mierzwa@gmail.com>
2020-07-21 10:21:14 +0200

    Update checkbox example

--- R/checkbox.R
+++ R/checkbox.R
@@ -11,20 +11,22 @@
 #' @param style Style of the widget.
 #'
 #' @examples
-#' checkbox_input("example", "Check me", is_marked = FALSE)
-#'
 #' if (interactive()){
-#' ui <- semanticPage(
-#'  shinyUI(
-#'    toggle("tog1", "My Label", TRUE)
-#'  )
-#' )
-#' server <- function(input, output, session) {
-#'   observeEvent(input$tog1, {
-#'     print(input$tog1)
-#'   })
-#' }
-#' shinyApp(ui, server)
+#'   ui <- shinyUI(
+#'     semanticPage(
+#'       p("Simple checkbox:"),
+#'       checkbox_input("example", "Check me", is_marked = FALSE),
+#'       p(),
+#'       p("Simple toggle:"),
+#'       toggle("tog1", "My Label", TRUE)
+#'     )
+#'   )
+#'   server <- function(input, output, session) {
+#'     observeEvent(input$tog1, {
+#'       print(input$tog1)
+#'     })
+#'   }
+#'   shinyApp(ui, server)
 #' }
 #' @details
 #' The inputs are updateable by using \code{\link[shiny]{updateCheckboxInput}}.
