3bbd866b24b81eaaf14421a85e5c9527fcf21964
Jakub Chojna <jakub.chojna@appsilon.com>
2020-07-21 08:15:02 +0200

    feat: add container and areas styling to sidebar_layout

--- examples/sidebar_layout/app.R
+++ examples/sidebar_layout/app.R
@@ -37,13 +37,17 @@ main_panel <- function(grid, ...) {
   # adjustments
 }
 
-# Sidebar Layout ...............................................................
-sidebar_layout <- function(sidebar_panel,
-                          main_panel,
-                          sidebar_width,
-                          mirrored = FALSE,
-                          min_height = "auto") {
+# Sidebar Layout ---------------------------------------------------------------
 
+sidebar_layout <- function(sidebar_panel,
+                           main_panel,
+                           sidebar_width,
+                           mirrored = FALSE,
+                           min_height = "auto",
+                           container_style,
+                           area_styles) {
+
+  # set normal or mirrored sidebar layout
   if (!mirrored) {
     layout <- grid_template(default = list(
       areas = rbind(c("sidebar_panel", "main_panel")),
@@ -56,16 +60,23 @@ sidebar_layout <- function(sidebar_panel,
     ))
   }
 
+  # grid container's default styling
   container_style <- glue::glue("
-    align-content: start;
-    border: 2px dotted black;
     height: auto;
     min-height: {min_height};
+    {container_style}
   ")
 
+  # grid container's children default styling
   area_styles <- list(
-    sidebar_panel = "",
-    main_panel = ""
+    sidebar_panel = glue::glue("
+      border: 1px solid orange;
+      {area_styles$sidebar_panel}
+    "),
+    main_panel = glue::glue("
+      border: 1px solid blue;
+      {area_styles$main_panel}
+    ")
   )
 
   grid(
@@ -77,7 +88,7 @@ sidebar_layout <- function(sidebar_panel,
   )
 }
 
-# UI ...........................................................................
+# UI ---------------------------------------------------------------------------
 ui <- function() {
   shinyUI(semanticPage(
     title = "Sidebar Layout Test",
@@ -97,31 +108,65 @@ ui <- function() {
       #   "Main 4"
       # ),
       sidebar_panel(
-        grid = NULL,
-        "Side Item 1",
-        "Side Item 2",
-        "Side Item 3"
+        grid = list(
+          layout = grid_template(default = list(
+            areas = rbind(
+              c("top_left",    "top_right"   ),
+              c("bottom_left", "bottom_right")
+            ),
+            cols_width = c("1fr", "2fr"),
+            rows_height = c("2fr", "3fr")
+          )),
+          container_style = "border: 5px solid tomato;background-color: black;",
+          area_styles = list(
+            bottom_left = "background-color: yellow;",
+            bottom_right = "background-color: teal;"
+          )
+        ),
+        top_left = "Top left part of sidebar",
+        top_right = "Top right part of sidebar",
+        bottom_left = "Bottom left part of sidebar",
+        bottom_right = "Bottom right part of sidebar"
       ),
       main_panel(
-        grid = grid(
-          grid_template = grid_template(default = list(
-            areas = rbind(c("test1", "test2"), c("test3", "test4")),
-            cols_width = c("1fr", "1fr")
+        grid = list(
+          layout = grid_template(default = list(
+            areas = rbind(
+              c("top_left",    "top_center",    "top_right"   ),
+              c("middle_left", "middle_center", "middle_right"),
+              c("bottom_left", "bottom_center", "bottom_right")
+            ),
+            cols_width = c("1fr", "1fr"),
+            rows_height = c("100px", "1fr", "30%")
           )),
-          container_style = "border: 2px dotted black;",
-          test1 = "test-1",
-          test2 = "test-2",
-          test3 = "test-3",
-          test4 = "test-4"
-        )
+          container_style = "border: 5px solid cyan; color: black",
+          area_styles = list(
+            top_left = "background-color: purple;",
+            middle_center = "background-color: tomato;"
+          )
+        ),
+        top_left = "Top left part of main panel",
+        top_center = "Top center part of main panel",
+        top_right = "Top right part of main panel",
+        middle_left = "Middle left part of main panel",
+        middle_center = "Middle center part of main panel",
+        middle_right = "Middle right part of main panel",
+        bottom_left = "Bottom left part of main panel",
+        bottom_center = "Bottom center part of main panel",
+        bottom_right = "Bottom right part of main panel"
       ),
-      sidebar_width = 30,
+      sidebar_width = 20,
       min_height = "400px",
-      mirrored = FALSE
+      mirrored = FALSE,
+      container_style = "background-color: white;",
+      area_styles = list(
+        sidebar_panel = "border: 5px solid orange; color: white;",
+        main_panel = "border: 5px solid hotpink;"
+      )
     )
   ))
 }
 
-# SERVER .......................................................................
+# SERVER -----------------------------------------------------------------------
 server <- shinyServer(function(input, output) {})
 shinyApp(ui = ui(), server = server)
