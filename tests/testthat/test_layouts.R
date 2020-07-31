44af48a8d7f94337c57a37ab8b3a679e359ebcd4
Dominik Krzeminski <dominik@appsilon.com>
2020-07-31 15:59:51 +0100

    [add] split_layout added with tests

--- tests/testthat/test_layouts.R
+++ tests/testthat/test_layouts.R
@@ -41,3 +41,27 @@ test_that("test sidebar_layout", {
   # empty input
   expect_is(sidebar_layout(sidebar_panel(),main_panel()), "list")
 })
+
+test_that("test split_layout", {
+  # empty imput gives error
+  expect_error(sidebar_layout())
+  # simple input
+  si_str <- as.character(split_layout(p("a")))
+  expect_true(any(grepl("<p>a</p>", si_str, fixed = TRUE)))
+  expect_true(any(grepl("col1", si_str, fixed = TRUE)))
+  expect_false(any(grepl("col2", si_str, fixed = TRUE)))
+
+  # width of columns (number)
+  si_str <- as.character(split_layout(
+    cell_widths = 300,
+    cell_args = "padding: 6px;",
+    p("p1")
+  ))
+  expect_true(any(grepl("grid-template-columns: 300", si_str, fixed = TRUE)))
+  # width of columns (vector)
+  si_str <- as.character(
+    split_layout(cell_widths = c("25%", "75%"), p("p1"), p("p2"))
+  )
+  expect_true(any(grepl("grid-template-columns: 25% 75%", si_str, fixed = TRUE)))
+
+})
