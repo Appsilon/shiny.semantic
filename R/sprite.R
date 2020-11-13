2efa17b8a4eb44d4cd92e7a85eec8812ca41f88c
osenan <oriol.senan@praenoscere.com>
2020-11-13 20:12:28 +0100

    fix: Reduce size of svg examples

--- R/sprite.R
+++ R/sprite.R
@@ -1,5 +1,5 @@
 idExists <- function(id, sprite) {
-    sprite_sym <- xml_text(xml_find_all(
+    sprite_sym <- xml2::xml_text(xml2::xml_find_all(
         sprite, "/*[name()='svg']/*[name()='symbol']/@id"))
     if(id %in% sprite_sym) return(TRUE) else return(FALSE)
 }
@@ -12,13 +12,15 @@ idExists <- function(id, sprite) {
 #' NULL it looks for the id inside the \code{newsvg} object.
 #'
 #' @return invisible copy of new sprite map
-#' 
+#'
 #' @details the sprite map object is modified in place
 #'
 #' @examples
 #' library(xml2)
 #' sprite <- xml_new_root("svg", "version" = "1.1", "xmlns" = "http://www.w3.org/2000/svg")
-#' newsvg <- read_xml('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"> <g> <path d="M50.049,0.3c14.18,0.332,25.969,5.307,35.366,14.923S99.675,36.9,100,51.409c-0.195,11.445-3.415,21.494-9.658,30.146 - yadda yadda yadda"/> </g> </svg>')
+#' newsvg <- read_xml("<svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 68'>
+#' <path d='M55.096 10.462c1.338-1.374.14-3.699-1.322-3.136-1.482.572-1'/>
+#'    </svg>")
 #' addsvg_sprite(newsvg, sprite, "new-icon")
 #' @export
 addsvg_sprite <- function(newsvg, sprite, id = NULL) {
@@ -27,16 +29,16 @@ addsvg_sprite <- function(newsvg, sprite, id = NULL) {
              Please install it", call. = FALSE)
     }
     if(is.null(id)) {
-        id <- xml_attr(newsvg, "id")
+        id <- xml2::xml_attr(newsvg, "id")
         if(is.na(id)) stop("id is NULL and newsvg does not have id")
     }
     stopifnot(!idExists(id, sprite))
-    viewBox <- xml_attr(newsvg, "viewBox")
-    root_newsvg <- xml_new_root("symbol", "id" = id,
+    viewBox <- xml2::xml_attr(newsvg, "viewBox")
+    root_newsvg <- xml2::xml_new_root("symbol", "id" = id,
         "viewBox" = viewBox)
-    xml_add_child(root_newsvg, xml_child(newsvg))
-    xml_add_child(sprite,
-         xml_find_first(root_newsvg, "/*[name()='symbol']"))
+    xml2::xml_add_child(root_newsvg, xml2::xml_child(newsvg))
+    xml2::xml_add_child(sprite,
+         xml2::xml_find_first(root_newsvg, "/*[name()='symbol']"))
     invisible(sprite)
 }
 
@@ -53,7 +55,9 @@ addsvg_sprite <- function(newsvg, sprite, id = NULL) {
 #' @examples
 #' library(xml2)
 #' sprite <- xml_new_root("svg", "version" = "1.1", "xmlns" = "http://www.w3.org/2000/svg")
-#' newsvg <- read_xml("<svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'> <g> <path d='M50.049,0.3c14.18,0.332,25.969,5.307,35.366,14.923S99.675,36.9,100,51.409c-0.195,11.445-3.415,21.494-9.658,30.146 - yadda yadda yadda'/> </g> </svg>")
+#' newsvg <- read_xml("<svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 68'>
+#' <path d='M55.096 10.462c1.338-1.374.14-3.699-1.322-3.136-1.482.572-1'/>
+#' </svg>")
 #' addsvg_sprite(newsvg, sprite, "new-icon")
 #' rmsvg_sprite("new-icon", sprite)
 #' @export
@@ -63,10 +67,10 @@ addsvg_sprite <- function(newsvg, sprite, id = NULL) {
              Please install it", call. = FALSE)
     }
     stopifnot(idExists(id, sprite))
-    sym2remove <- xml_find_first(sprite,          
+    sym2remove <- xml2::xml_find_first(sprite,
         paste0("/*[name()='svg']/*[name()='symbol'][@id='",id,"']"))
-    xml_remove(sym2remove)
+    xml2::xml_remove(sym2remove)
     invisible(sprite)
 }
 
-    
+
