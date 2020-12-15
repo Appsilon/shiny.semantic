3b2359c3f1e6ff4434f19126bb91d13b57e7b0aa
rabiibouhestine <rabiibouhestine@outlook.com>
2020-12-15 12:42:55 +0100

    fixed tab content not rendering

--- examples/tabset/app.R
+++ examples/tabset/app.R
@@ -5,13 +5,20 @@ ui <- shinyUI(semanticPage(
   h2("Active Tab:"),
   textOutput("activetab"),
   action_button("changetab", "Select First Tab!"),
+  selectInput(
+    inputId = "plot1xaxis",
+    label = "change x axis variable in third tab:",
+    choices = names(mtcars),
+    selected = "mpg",
+  ),
+
   tabset(tabs =
            list(
              list(menu = "First Tab", content = "Text works well", id= "first_tab"),
              list(menu = "Second Tab", content = plotOutput("plot1"), id = "second_tab"),
              list(menu = "Third Tab", content = plotOutput("plot2"), id = "third_tab")
            ),
-         active = "third_tab",
+         active = "second_tab",
          id = "exampletabset"
   )
 ))
@@ -21,8 +28,16 @@ server <- function(input, output, session) {
     update_tabset(session, "exampletabset", "first_tab")
   })
 
-  output$plot1 <- renderPlot(plot(c(1, 2), c(1, 5), type='b', main = "Plot 1"))
-  output$plot2 <- renderPlot(plot(mtcars$mpg, mtcars$cyl, col = 'red'))
+  output$plot1 <- renderPlot(
+    plot(c(1, 2), c(1, 5), type='b', main = "Plot 1",
+         xlab = "x axis", ylab = "y axis")
+  )
+
+  output$plot2 <- renderPlot(
+    plot(mtcars[[input$plot1xaxis]], mtcars$cyl, col = 'red',
+         xlab = input$plot1xaxis, ylab = "cyl")
+  )
+
   output$activetab <- renderText(input$exampletabset)
 }
 
--- inst/www/shiny-semantic-tabset.js
+++ inst/www/shiny-semantic-tabset.js
@@ -17,6 +17,7 @@ $.extend(semanticTabset, {
       callback();
     });
     $(el).find('.item').on('click', function(event) {
+      $(this).trigger('shown');
       callback();
     });
   },
