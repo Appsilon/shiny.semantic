30803f1480060dda67d438961127b7073b4e406c
rabiibouhestine <rabiibouhestine@outlook.com>
2020-12-15 15:04:11 +0100

    added mobile grid in example

--- examples/grid/app.R
+++ examples/grid/app.R
@@ -2,30 +2,55 @@ library(shiny)
 library(shiny.semantic)
 
 # Here we define the grid_template that we will pass to our grid
-myGrid <- grid_template(default = list(
-  # Here we define the data.frame describing our layout
-  # The easiest way is to use rbind so that the layout can be 'visualized' in code
-  areas = rbind(
-    c("header", "header", "header"),
-    c("menu",   "main",   "right1"),
-    c("menu",   "main",   "right2")
+myGrid <- grid_template(
+  default = list(
+    # Here we define the data.frame describing our layout
+    # The easiest way is to use rbind so that the layout can be 'visualized' in code
+    areas = rbind(
+      c("header", "header", "header"),
+      c("menu",   "main",   "right1"),
+      c("menu",   "main",   "right2")
+    ),
+    # Then we define the dimensions of the different elements of the layout
+    # We can use any valid css units to make the layout behave exactly as desired
+    rows_height = c("50px", "auto", "100px"),
+    cols_width = c("100px", "2fr", "1fr")
   ),
-  # Then we define the dimensions of the different elements of the layout
-  # We can use any valid css units to make the layout behave exactly as desired
-  rows_height = c("50px", "auto", "100px"),
-  cols_width = c("100px", "2fr", "1fr")
-))
+  # This is optional, but we can define a specific layout for mobile (screen width below 768px)
+  mobile = list(
+    areas = rbind(
+      c("header", "header", "header"),
+      c("menu",   "main",   "right1"),
+      c("menu",   "main",   "right2")
+    ),
+    rows_height = c("50px", "2fr", "1fr"), # Notice how we changed the rows heights here
+    cols_width = c("100px", "2fr", "1fr")
+  )
+)
 
 # We can use nested grids to precisely control the layout of all the elements
 # Here we define another grid_template to use in one of the elements of the parent grid
-subGrid <- grid_template(default = list(
-  areas = rbind(
-    c("top_left", "top_right"),
-    c("bottom_left", "bottom_right")
+subGrid <- grid_template(
+  default = list(
+    areas = rbind(
+      c("top_left", "top_right"),
+      c("bottom_left", "bottom_right")
+    ),
+    rows_height = c("50%", "50%"),
+    cols_width = c("50%", "50%")
   ),
-  rows_height = c("50%", "50%"),
-  cols_width = c("50%", "50%")
-))
+  # This is optional, but we can define a specific layout for mobile (screen width below 768px)
+  mobile = list(
+    areas = rbind(
+      c("top_left"),
+      c("top_right"),
+      c("bottom_left"),
+      c("bottom_right")
+    ),
+    rows_height = c("25%", "25%", "25%", "25%"),
+    cols_width = c("100%")
+  )
+)
 
 ui <- semanticPage(
   grid(myGrid,
