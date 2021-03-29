ce3ec25c8225a5313c8f1704e279b725fa8bf7f4
Ashley Baldry <arbaldry91@gmail.com>
2021-03-29 10:26:20 +0100

    Unbinding progress bar as an input. Fixes #359

--- inst/www/shiny-semantic-progress.js
+++ inst/www/shiny-semantic-progress.js
@@ -1,97 +1,20 @@
 // Shiny input for progress bars
-var semanticProgressBinding = new Shiny.InputBinding();
-
-$.extend(semanticProgressBinding, {
-
-  // This initialize input element. It extracts data-value attribute and use that as value.
-  initialize: function(el) {
-    $(el).progress({
-      text: {
-        active: $(el).data('label'),
-        success: $(el).data('label-complete')
-      }
-    });
-  },
-
-  // This returns a jQuery object with the DOM element.
-  find: function(scope) {
-    return $(scope).find('.ss-progress');
-  },
-
-  // Returns the ID of the DOM element.
-  getId: function(el) {
-    return el.id;
-  },
-
-  // Given the DOM element for the input, return the value as JSON.
-  getValue: function(el) {
-    if ($(el).data('value')) {
-      return $(el).progress('get value');
-    } else {
-      return $(el).progress('get percent');
+renderProgressBars = function() {
+  $('.progress').progress({
+    text: {
+      active: $(this).data('label'),
+      success: $(this).data('label-complete')
+    },
+    onActive: function() {
+      Shiny.setInputValue(this.id, $(this).progress('get value'));
+    },
+    onChange: function() {
+      Shiny.setInputValue(this.id, $(this).progress('get value'));
     }
-  },
+  });
+};
 
-  // Given the DOM element for the input, set the value.
-  setValue: function(el, value) {
-    if ($(el).data('value')) {
-      return $(el).progress('set progress', value);
-    } else {
-      return $(el).progress('set percent', value);
-    }
-  },
-
-  // Set up the event listeners so that interactions with the
-  // input will result in data being sent to server.
-  // callback is a function that queues data to be sent to
-  // the server.
-  subscribe: function(el, callback) {
-    $(el).on('keyup change', function () { callback(true); });
-  },
-
-  // TODO: Remove the event listeners.
-  unsubscribe: function(el) {
-    $(el).off('.semanticProgressBinding');
-  },
-
-  // This returns a full description of the input's state.
-  getState: function(el) {
-    return {
-      value: this.getValue(el)
-    };
-  },
-
-  // The input rate limiting policy.
-  getRatePolicy: function() {
-    return {
-      // Can be 'debounce' or 'throttle':
-      policy: 'debounce',
-      delay: 50
-    };
-  },
-
-  receiveMessage: function(el, data) {
-    if (data.hasOwnProperty('value')) {
-      this.setValue(el, data.value);
-    }
-
-    if (data.hasOwnProperty('label')) {
-      $(el).progress('set label', data.label);
-    }
-
-    if (data.hasOwnProperty('increment')) {
-      $(el).progress('increment', Number(data.increment));
-    }
-
-    if (data.hasOwnProperty('decrement')) {
-      $(el).progress('decrement', Number(data.decrement));
-    }
-
-    $(el).trigger('change');
-  }
-});
-
-Shiny.inputBindings.register(semanticProgressBinding, 'shiny.semanticProgress');
+$(window).on('load', renderProgressBars);
 
 // JS to handle the Progress object similar to shiny
 Shiny.addCustomMessageHandler('ssprogress', function(message) {
@@ -125,6 +48,18 @@ var ssProgressHandlers = {
     $(`#ss-progress-${message.id}`).progress();
   },
 
+  change: function(message) {
+    var progress = $('#' + message.id);
+
+    if (message.type === 'label') {
+      progress.progress('set label', message.value);
+    } else if (message.type === 'value') {
+      progress.progress('set progress', message.value);
+    } else {
+      progress.progress(message.type, message.value);
+    }
+  },
+
   // Update page-level progress bar
   update: function(message) {
     // For new-style (starting in Shiny 0.14) progress indicators that use
@@ -139,7 +74,7 @@ var ssProgressHandlers = {
     }
     if (typeof(message.value) !== 'undefined' && message.value !== null) {
       progress.progress('set progress', message.value);
-      }
+    }
 
   },
 
