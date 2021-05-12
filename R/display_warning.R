3f5a863ae5621007c4f5f369f0cf2f7332320fa6
rabiibouhestine <rabiibouhestine@outlook.com>
2021-05-12 15:20:20 +0100

    fixed fct names in fct docs

--- R/display_warning.R
+++ R/display_warning.R
@@ -12,31 +12,31 @@ basicPage <- function(...) {
   shiny::basicPage(...)
 }
 
-#' Generates warning message if basicPage is used.
+#' Generates warning message if bootstrapPage is used.
 bootstrapPage <- function(...) {
   warning(page_display_warning("bootstrapPage"))
   shiny::bootstrapPage(...)
 }
 
-#' Generates warning message if basicPage is used.
+#' Generates warning message if fillPage is used.
 fillPage <- function(...) {
   warning(page_display_warning("fillPage"))
   shiny::fillPage(...)
 }
 
-#' Generates warning message if basicPage is used.
+#' Generates warning message if fixedPage is used.
 fixedPage <- function(...) {
   warning(page_display_warning("fixedPage"))
   shiny::fixedPage(...)
 }
 
-#' Generates warning message if basicPage is used.
+#' Generates warning message if navbarPage is used.
 navbarPage <- function(...) {
   warning(page_display_warning("navbarPage"))
   shiny::navbarPage(...)
 }
 
-#' Generates warning message if basicPage is used.
+#' Generates warning message if fluidPage is used.
 fluidPage <- function(...) {
   warning(page_display_warning("fluidPage"))
   shiny::fluidPage(...)
