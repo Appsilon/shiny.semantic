0f3f8d32766dafb93372ffe73aac76e0a9a7bb9b
rabiibouhestine <rabiibouhestine@outlook.com>
2021-05-12 13:42:27 +0100

    show warning if default shiny page fct is used

---
+++ R/display_warning.R
@@ -0,0 +1,43 @@
+#' Generates warning message.
+#'
+#' @param fct Name of the shiny function used.
+page_display_warning <- function(fct) {
+  glue::glue("shiny.semantic is loaded but {fct} is used. This will cause display issues.
+             Please unload shiny.semantic or use semanticPage() instead.")
+}
+
+#' Generates warning message if basicPage is used.
+basicPage <- function(...) {
+  warning(page_display_warning("basicPage"))
+  shiny::basicPage(...)
+}
+
+#' Generates warning message if basicPage is used.
+bootstrapPage <- function(...) {
+  warning(page_display_warning("bootstrapPage"))
+  shiny::bootstrapPage(...)
+}
+
+#' Generates warning message if basicPage is used.
+fillPage <- function(...) {
+  warning(page_display_warning("fillPage"))
+  shiny::fillPage(...)
+}
+
+#' Generates warning message if basicPage is used.
+fixedPage <- function(...) {
+  warning(page_display_warning("fixedPage"))
+  shiny::fixedPage(...)
+}
+
+#' Generates warning message if basicPage is used.
+navbarPage <- function(...) {
+  warning(page_display_warning("navbarPage"))
+  shiny::navbarPage(...)
+}
+
+#' Generates warning message if basicPage is used.
+fluidPage <- function(...) {
+  warning(page_display_warning("fluidPage"))
+  shiny::fluidPage(...)
+}
