7361304c688a0b89e9f377a00347d1c40dfc9ee2
Ashley Baldry <arbaldry91@gmail.com>
2021-10-17 17:24:33 +0100

    First attempt of form validation

---
+++ R/form.R
@@ -0,0 +1,65 @@
+#' Form Validation for Semantic UI
+#'
+#' @seealso field_validation
+#'
+#' @export
+form_validation <- function(id, ...) {
+  fields <- list(...)
+  names(fields) <- vapply(fields, function(x) x$identifier, character(1))
+  fields_json <- jsonlite::toJSON(fields, auto_unbox = TRUE)
+
+  tags$script(glue::glue("$('#{|id|}').form({fields: {|fields_json|}});", .open = "{|", .close = "|}"))
+}
+
+#' Field Validation for Semantic UI
+#'
+#' @param id HTML id of the field to be validated
+#' @param ... A series of \code{field_rule}s that will be applied to the field
+#'
+#' @details
+#' The following \code{rule} are available:
+#' \itemize{
+#' }
+#'
+#' @examples
+#' # E-mail validations
+#' field_validation("email", field_rule("email"))
+#'
+#' @seealso \code{form_validation}
+#'
+#' @rdname field_validation
+#' @export
+field_validation <- function(id, ...) {
+  rules <- list(...)
+  structure(list(identifier = id, rules = rules), class = c("list", "field_validation"))
+}
+
+#' @param rule The type of rule to be applied. Valid rules are available in Details.
+#' @param prompt Text to be displayed in the UI if the validation fails. Leave \code{NULL} if want to use default text.
+#' @param value Certain fields require a value to check validation. Check Details if the \code{rule} requires a value.
+#'
+#' @rdname field_validation
+#' @export
+field_rule <- function(rule, prompt = NULL, value = NULL) {
+  if (!rule %in% FIELD_RULES) stop("Field rule must be one of: ", paste(FIELD_RULES, collapse = ", "))
+
+  if (rule %in% FIELD_RULES_NO_VALUES && is.null(value)) {
+    rule_info <- list(type = rule)
+  } else {
+    if (is.null(value)) stop(rule, " rule must have a value for validation")
+    rule_info <- list(type = paste0(rule, "[", value, "]"))
+  }
+
+  if (!is.null(prompt)) rule_info$prompt <- prompt
+
+  structure(rule_info, class = c("list", "rule_info"))
+}
+
+FIELD_RULES_NO_VALUES <- c("empty", "checked", "email", "url", "integer", "decimal", "number", "creditCard")
+FIELD_RULES_WITH_VALUES <- c(
+  "integer", "decimal", "number", "minValue", "maxValue", "regExp", "creditCard",
+  "contains", "containsExactly", "doesntContain", "doesntContainExactly", "is", "isExactly",
+  "not", "notExactly", "minLength", "exactLength", "maxLength", "match", "different",
+  "minCount", "exactCount", "maxCount"
+)
+FIELD_RULES <- unique(c(FIELD_RULES_NO_VALUES, FIELD_RULES_WITH_VALUES))
---
+++ examples/form_validation/app.R
@@ -0,0 +1,136 @@
+library(shiny)
+library(shiny.semantic)
+
+ui <- shinyUI(
+  semanticPage(
+    tags$br(),
+
+    div(
+      class = "ui grid",
+      div(
+        class = "two column row",
+        # Form Input
+        div(
+          class = "column",
+          segment(
+            form(
+              id = "form",
+              h4(class = "ui dividing header", "Inputs"),
+              field(
+                tags$label("Text"),
+                text_input("text_ex", value = "", type = "text", placeholder = "Enter Text...")
+              ),
+              field(
+                tags$label("Text Area"),
+                text_input(
+                  "textarea_ex", value = "", type = "textarea", placeholder = "Enter Text...", attribs = list(rows = 2)
+                )
+              ),
+              field(
+                tags$label("Password"),
+                text_input("password_ex", value = "", type = "password", placeholder = "Select Password")
+              ),
+              field(
+                tags$label("E-Mail"),
+                text_input("email_ex", value = "", type = "email", placeholder = "Enter E-Mail")
+              ),
+              field(
+                tags$label("URL"),
+                text_input("url_ex", value = "", type = "url", placeholder = "Enter URL")
+              ),
+              field(
+                numeric_input("number_ex", "Numeric", value = 50, min = 0, max = 100)
+              ),
+              field(
+                tags$label("Checkbox"),
+                checkbox_input("checkbox_ex", "Checkbox", is_marked = FALSE),
+              ),
+              field(
+                tags$label("Group Radio Button"),
+                multiple_radio(
+                  "grp_radio_ex", "Favourite Letter", choices = LETTERS[1:4], selected = "B", position = "inline"
+                )
+              ),
+              action_button(input_id = "submit", label = "Submit", class = "submit"),
+              div(class = "ui error message"),
+              form_validation(
+                "form",
+                field_validation("password_ex", field_rule("minLength", value = 6)),
+                field_validation("email_ex", field_rule("email")),
+                field_validation("checkbox_ex", field_rule("checked"))
+              )
+            )
+          )
+        ),
+
+        # Form Output
+        div(
+          class = "column",
+          segment(
+            form(
+              h4(class = "ui dividing header", "Outputs"),
+              field(
+                tags$label("Text"),
+                "Written Text: ", shiny::textOutput("text_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("Text Area"),
+                "Written Text: ", shiny::textOutput("textarea_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("Password"),
+                "Written Text: ", shiny::textOutput("password_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("E-Mail"),
+                "Written Text: ", shiny::textOutput("email_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("URL"),
+                "Written Text: ", shiny::textOutput("url_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("Number"),
+                "Selected Number: ", shiny::textOutput("number_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("Checkbox"),
+                "Checkbox Selected:", shiny::textOutput("checkbox_ex", container = shiny::span),
+                tags$br(),
+                "Slider Selected:", shiny::textOutput("slider_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("Group Radio Button"),
+                "Radio Button Selected:", shiny::textOutput("grp_radio_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("Group Checkbox"),
+                "Checkboxes Selected:", shiny::textOutput("grp_check_ex", container = shiny::span)
+              ),
+              field(
+                tags$label("Calendar"),
+                "Date Selected:", shiny::textOutput("calendar_ex", container = shiny::span)
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+)
+
+server <- shinyServer(function(input, output, session) {
+  output$text_ex <- renderText(input$text_ex)
+  output$textarea_ex <- renderText(input$textarea_ex)
+  output$password_ex <- renderText(input$password_ex)
+  output$email_ex <- renderText(input$email_ex)
+  output$url_ex <- renderText(input$url_ex)
+  output$number_ex <- renderText(paste(input$number_ex, "   Class: ", class(input$number_ex)))
+  output$checkbox_ex <- renderText(input$checkbox_ex)
+  output$slider_ex <- renderText(input$slider_ex)
+  output$grp_radio_ex <- renderText(input$grp_radio_ex)
+  output$grp_check_ex <- renderText(paste(input$grp_check_ex, collapse = ", "))
+  output$calendar_ex <- renderText(as.character(input$calendar_ex))
+})
+
+shiny::shinyApp(ui, server)
