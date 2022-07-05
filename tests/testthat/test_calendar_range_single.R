2bce526fd04f3d063dceceeab81236c40de52393
Esra Ay <esra@pranageo.com>
2022-07-05 20:47:10 +0300

    Add new test for single calendar range

---
+++ tests/testthat/test_calendar_range_single.R
@@ -0,0 +1,22 @@
+context("ui calendar")
+
+test_that("test calendar", {
+  # type
+  expect_is(calendar_range_single("calendar_input"), "shiny.tag")
+  # empty input
+  expect_error(calendar_range_single())
+  # number input
+  si_str <- as.character(calendar_range_single("calendar_input"))
+  expect_true(grepl("<div id=\"calendar_input\" class=\"ui calendar ss-input-date\" data-type=\"date\">",
+                    si_str))
+  # all parameters
+  expect_is(
+    calendar_range_single("calendar_input", Sys.Date(),
+             "Select tomorrow",
+             min = Sys.Date() - 3,
+             max = Sys.Date() + 4,
+             start_calendar_id = "start_calendar_id",
+             end_calendar_id = "end_calendar_id"),
+    "shiny.tag"
+  )
+})
