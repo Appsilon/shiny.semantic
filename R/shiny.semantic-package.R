ef44fb902b83d507b5095d1d18897dd4291c1b64
Jakub Nowicki <kuba@appsilon.com>
2024-04-04 12:12:43 +0200

    docs: Update package documentation to follow the roxygen2 7 setup.

---
+++ R/shiny.semantic-package.R
@@ -0,0 +1,21 @@
+#' Semantic UI wrapper for Shiny
+#'
+#'
+#' @description With this library it’s easy to wrap Shiny with Semantic UI
+#' components. Add a few simple lines of code and some CSS classes to give
+#' your UI a fresh, modern and highly interactive look.
+#'
+#' @section Options:
+#' There are a number of global options that affect shiny.semantic as well as
+#' Shiny behavior.The options can be set globally with `options()`
+#' \describe{
+#' \item{shiny.custom.semantic.cdn (defaults to `NULL`)}{This controls from where the css
+#' and javascripts will be downloaded.}
+#' \item{shiny.custom.semantic (defaults to `NULL`)}{This allows to set custom local path
+#' to semantic dependencies.}
+#' \item{shiny.minified (defaults to `TRUE`)}{Defines including JavaScript as a minified or
+#' un-minified file.}
+#' }
+#'
+#' @keywords internal
+"_PACKAGE"
